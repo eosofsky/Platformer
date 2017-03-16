681a3a1f6baf079149234f3c1102b73e108576c6
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-03-15 20:55:43 -0400

    Can now switch hands.

---
+++ Platformer/Assets/Scripts/Character/SwitchHands.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHands : MonoBehaviour {
+
+	public static bool HasOriginalHands = true;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.O)) {
+			Switch ();
+		}
+	}
+
+	public static void Switch () {
+		GameObject[] originalHands = GameObject.FindGameObjectsWithTag ("OriginalHand");
+		for (int i = 0; i < originalHands.Length; i++) {
+			originalHands [i].GetComponent <SkinnedMeshRenderer> ().enabled = !HasOriginalHands;
+		}
+
+		GameObject[] altHands = GameObject.FindGameObjectsWithTag ("AltHand");
+		for (int i = 0; i < originalHands.Length; i++) {
+			altHands [i].GetComponent <SkinnedMeshRenderer> ().enabled = HasOriginalHands;
+		}
+
+		HasOriginalHands = !HasOriginalHands;
+	}
+}
