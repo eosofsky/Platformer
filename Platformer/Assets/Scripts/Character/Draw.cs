27cceb2722f9aafe3816697174938a4befda8fd3
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-03-22 02:38:28 -0400

    Made the drawer. Also the protagonist can do a drawing animation if he walks up to the lever operator.

---
+++ Platformer/Assets/Scripts/Character/Draw.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour {
+
+	private Animator animator;
+	private static bool canDraw = false;
+
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+
+	public static void SetDrawable (bool drawable) {
+		canDraw = drawable;
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (canDraw && collider.name == "Lever Operator") {
+			animator.SetTrigger ("Draw");
+		}
+	}
+}
---
+++ Platformer/Assets/Scripts/Environment/StairsButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsButton : MonoBehaviour {
+
+	public Material activateMat;
+	public Material deactivateMat;
+
+	private static int numHolders = 0;
+
+	private MeshRenderer renderer;
+
+	void Awake () {
+		renderer = GetComponent<MeshRenderer> ();
+	}
+
+	void OnTriggerEnter () {
+		renderer.material = activateMat;
+		numHolders++;
+	}
+		
+	void OnTriggerExit () {
+		numHolders--;
+		if (numHolders == 0) {
+			renderer.material = deactivateMat;
+		}
+	}
+
+}
