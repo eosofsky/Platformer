f52617ca43598462f50d0a2724cfb6bd036e38e7
Everi Osofsky <eosofsky@andrew.cmu.edu>
2017-03-21 21:33:10 -0400

    Made two other characters- the lever operator, who pushes a lever, and the wheelbarrow guy, who walks between the "furnace" and the "coal."

---
+++ Platformer/Assets/Scripts/Character/LeverOperator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverOperator : MonoBehaviour {
+
+	public float leverRotAmount = 0.1f;
+	public float leverRotMax = 0f;
+	public float pushDelay;
+	public float pullDelay;
+
+	private Animator animator;
+	private bool pushed = false;
+	private GameObject leverHandle;
+	private bool isPushing = false;
+	private bool isPulling = false;
+	private Quaternion startRot;
+
+	void Awake () {
+		animator = GetComponent<Animator> ();
+		leverHandle = GameObject.Find ("Lever Handle");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.I)) {
+			PushOrPullLever ();
+		}
+
+		if (isPushing) {
+			Quaternion targetRot = leverHandle.transform.localRotation;
+			targetRot.x = leverRotMax;
+			leverHandle.transform.localRotation = Quaternion.Slerp (leverHandle.transform.localRotation, targetRot, leverRotAmount);
+			if (AlmostEqual (leverHandle.transform.localRotation.x, targetRot.x)) {
+				isPushing = false;
+			}
+		} else if (isPulling) {
+			leverHandle.transform.localRotation = Quaternion.Slerp (leverHandle.transform.localRotation, startRot, leverRotAmount);
+			if (AlmostEqual (leverHandle.transform.localRotation.x, startRot.x)) {
+				isPulling = false;
+			}
+		}
+	}
+
+
+	public void PushOrPullLever () {
+		if (pushed) {
+			StartCoroutine (PullLever ());
+		} else {
+			StartCoroutine (PushLever ());
+		}
+		pushed = !pushed;
+	}
+
+	IEnumerator PullLever () {
+		animator.SetTrigger ("Pull");
+		yield return new WaitForSeconds (pullDelay);
+		isPulling = true;
+	}
+
+	IEnumerator PushLever () {
+		animator.SetTrigger ("Push");
+		yield return new WaitForSeconds (pushDelay);
+		startRot = leverHandle.transform.localRotation;
+		isPushing = true;
+	}
+
+	private bool AlmostEqual(float x, float y)
+	{
+		float buffer = 0.01f;
+		float low = y - buffer;
+		float high = y + buffer;
+
+		return (low <= x && x <= high);
+	}
+
+}
---
+++ Platformer/Assets/Scripts/Character/WheelbarrowGuy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WheelbarrowGuy : MonoBehaviour {
+
+	public Transform coal;
+	public Transform furnace;
+	public float delay;
+
+	private Transform dest;
+	private Animator animator;
+
+	void Awake () {
+		dest = furnace;
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.destination = dest.position;
+
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.transform == coal) {
+			dest = furnace;
+		} else if (collision.transform == furnace) {
+			dest = coal;
+		} else {
+			return;
+		}
+		StartCoroutine (IdleAndGo ());
+	}
+
+	IEnumerator IdleAndGo () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.enabled = false;
+		animator.SetBool ("Idling", true);
+		yield return new WaitForSeconds (delay);
+		animator.SetBool ("Idling", false);
+		agent.enabled = true;
+		agent.destination = dest.position;
+	}
+
+}
