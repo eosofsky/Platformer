1e9bfc936a549cc24eb5357a5ea12d23b4a26503
bswalsh <bswalsh@andrew.cmu.edu>
2017-03-30 00:53:44 -0400

    added scripts to actually involve game aspects added with the environment

---
+++ Platformer/Assets/Scripts/PileController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PileController : MonoBehaviour {
+
+    public GameObject smallPiece;
+    public GameObject mediumPiece;
+    public GameObject largePiece;
+
+    public GameObject[] cells;
+
+	// Update is called once per frame
+	void Update () {
+        int WallsDestroyed = 0;
+        foreach (var wall in cells)
+        {
+            if (!wall.activeSelf)
+            {
+                WallsDestroyed++;
+            }
+        }
+        if (WallsDestroyed > 6)
+        {
+            largePiece.SetActive(false);
+        }
+        else if (WallsDestroyed > 3)
+        {
+            mediumPiece.SetActive(false);
+        }
+        else if (WallsDestroyed > 1)
+        {
+            smallPiece.SetActive(false);
+        }
+    }
+}
---
+++ Platformer/Assets/Scripts/WallDestroyer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroyer : MonoBehaviour {
+
+    public GameObject wall;
+    public GameObject[] wallPieces;
+
+    private int triggered;
+
+    private void Start()
+    {
+        triggered = 0;
+    }
+
+    private void Update()
+    {
+        if (triggered == 1)
+        {
+            foreach (var piece in wallPieces)
+            {
+                var rg = piece.AddComponent<Rigidbody>();
+                rg.useGravity = false;
+            }
+
+            triggered++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            triggered++;
+            wall.SetActive(false);
+        }
+    }
+}
